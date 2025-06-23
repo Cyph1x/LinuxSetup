4eaefd953dcd2c3067b44337c391c8a9de477bdb
Joshua Ward <42013718+Cyph1x@users.noreply.github.com>
2025-06-23 17:55:40 +1000

    Update rclone-install.sh
    fixed a bug

--- rclone-install.sh
+++ rclone-install.sh
@@ -7,8 +7,7 @@ mkdir /tmp/rclone-install
 wget https://downloads.rclone.org/rclone-current-linux-amd64.zip -O /tmp/rclone-install/rclone-current-linux-amd64.zip
 unzip /tmp/rclone-install/rclone-current-linux-amd64.zip -d /tmp/rclone-install
 
-
-cp /tmp/rclone-install/rclone-*-linux-amd64/rclone /usr/bin/
+sudo cp /tmp/rclone-install/rclone-*-linux-amd64/rclone /usr/bin/
 sudo chown root:root /usr/bin/rclone
 sudo chmod 755 /usr/bin/rclone
 
