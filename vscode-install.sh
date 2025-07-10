e4516e64dbcadfae524816916729888ffea67464
Joshua Ward <42013718+Cyph1x@users.noreply.github.com>
2025-07-10 11:44:08 +1000

    Update vscode-install.sh to use deb

--- vscode-install.sh
+++ vscode-install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-sudo snap install --classic code
+# sudo snap install --classic code
+mkdir /tmp/vscode-install
+
+curl -L -o /tmp/vscode-install/install.deb "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
+
+sudo apt --fix-broken install -y
+sudo dpkg -i /tmp/vscode-install/install.deb
+
+rm -rf /tmp/vscode-install
