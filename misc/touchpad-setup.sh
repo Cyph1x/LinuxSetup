b15b89b045b131d0c9074ebce6aa156680531f04
Cyph1x <42013718+Cyph1x@users.noreply.github.com>
2025-08-25 14:29:42 +1000

    Made touchpad setup script automatic

--- misc/touchpad-setup.sh
+++ misc/touchpad-setup.sh
@@ -2,9 +2,20 @@
 
 set -e
 
-xinput list
-read -p "Enter touchpad id: " touchpad_id
+sudo xinput list
+touchpad_line=$(xinput list | grep -i "touchpad" || true)
+
+if [[ -z "$touchpad_line" ]]; then
+    echo "Error: No touchpad found." >&2
+    exit 1
+fi
+
+touchpad_id=$(echo "$touchpad_line" | sed -n 's/.*id=\([0-9]\+\).*/\1/p')
+if [[ -z "$touchpad_id" ]]; then
+    echo "Error: Could not extract touchpad ID." >&2
+    exit 1
+fi
 
 echo "setting touchpad ($touchpad_id) sensitivity"
 
-xinput set-prop $touchpad_id "libinput Scrolling Pixel Distance" 30
+xinput set-prop $touchpad_id "libinput Scrolling Pixel Distance" 40
