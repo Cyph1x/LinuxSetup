019d2016fc909c8113475b3202cdb6e00c767622
Cyph1x <42013718+Cyph1x@users.noreply.github.com>
2025-08-25 15:55:51 +1000

    Added remaining packages to linux perf install

--- dev/linux-tools-install.sh
+++ dev/linux-tools-install.sh
@@ -11,13 +11,13 @@ sudo apt install linux-tools-common linux-tools-generic linux-tools-`uname -r` -
 # For some really stupid reason perf doesn't install
 # So we have to build from source
 # (See https://bugs.launchpad.net/ubuntu/+source/linux-hwe-6.14/+bug/2117159)
-sudo apt install build-essential git flex bison linux-headers-`uname -r` libelf-dev libpfm4-dev libtraceevent-dev asciidoc xmlto -y
+sudo apt install build-essential git flex bison linux-headers-`uname -r` libelf-dev libpfm4-dev libtraceevent-dev asciidoc xmlto libdw-dev libdebuginfod-dev systemtap-sdt-dev libslang2-dev libperl-dev python3-dev -y
 
 TEMP_DIR="/tmp/perf-install"
 echo "Temporary directory: $TEMP_DIR"
 
 mkdir -p "$TEMP_DIR"
-trap 'rm -rf "$TEMP_DIR"' EXIT
+trap 'sudo rm -rf "$TEMP_DIR"' EXIT
 
 git clone --depth 1 https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git "$TEMP_DIR/linux"
 
